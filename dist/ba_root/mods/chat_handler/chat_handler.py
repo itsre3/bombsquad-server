ce11737aa5dbbed3fd3f1de7f97ddccbdce2487a
itsre3 <105688454+itsre3@users.noreply.github.com>
2022-09-18 02:07:28 +0100

    Update chat_handler.py

--- dist/ba_root/mods/chat_handler/chat_handler.py
+++ dist/ba_root/mods/chat_handler/chat_handler.py
@@ -41,8 +41,7 @@ class check_perms:
                         ba.screenmessage("Chat Commands not enabled", color=(1, 0, 0), transient=True, clients=[self.client_id])
                         _ba.playsound(_ba.getsound("error"))
                         ret = None
-                else:
-                    return self.message
+                
             ret = None
         except:
             pass
