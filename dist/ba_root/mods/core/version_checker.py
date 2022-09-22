0615b7596640fa91d48c1f2092a96f772681a6e5
itsre3 <saturdayobokparomaxwell@gmail.com>
2022-09-22 01:45:24 +0100

    adding version checker

--- dist/ba_root/mods/core/version_checker.py
+++ dist/ba_root/mods/core/version_checker.py
@@ -3,9 +3,10 @@ import json
 import urllib.request
 import asyncio
 
+update_file = _ba.env["python_directory_user"], "updates.json"
 
 async def main():
-    with open("updates.json", "r") as f:
+    with open(update_file, "r") as f:
         prev = json.load(f)
     URL = "https://raw.githubusercontent.com/itsre3/bombsquad-server/main/dist/ba_root/mods/core/updates.json"
     resp = urllib.request.urlopen(URL)
