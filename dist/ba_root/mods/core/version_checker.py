d3c8f5eab75684676532ff6aec8cf1375d3337bd
itsre3 <saturdayobokparomaxwell@gmail.com>
2022-09-22 01:49:17 +0100

    adding version checker

--- dist/ba_root/mods/core/version_checker.py
+++ dist/ba_root/mods/core/version_checker.py
@@ -1,10 +1,11 @@
 
 import _ba
-import json
+import json, os
 import urllib.request
 import asyncio
 
-update_file = _ba.env["python_directory_user"], "updates.json"
+path = os.path.join(_ba.env["python_directory_user"], "core" + os.sep)
+update_file = path + "updates.json"
 
 async def main():
     with open(update_file, "r") as f:
