3a9938d419825bc82e2fd095f90199a6fe69d946
itsre3 <saturdayobokparomaxwell@gmail.com>
2023-01-23 20:31:08 +0000

    Flask errors

--- dist/ba_root/mods/web/webst.py
+++ dist/ba_root/mods/web/webst.py
@@ -1,19 +1,15 @@
 
 import ba
 import _ba
-import os
 import _thread
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-stats_dir = os.path.join(_ba.env()['python_directory_user'], "stats" + os.sep)
-
-stats_file = stats_dir + "stats_page.html"
 
 @app.route("/")
 def index():
-    return render_template(stats_file)
+    return render_template("stats_page.html")
 
 def run():
     _thread.start_new_thread(app.run, ("0.0.0.0", 5000, False))
