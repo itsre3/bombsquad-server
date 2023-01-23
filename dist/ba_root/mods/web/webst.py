97de97cffc1c35d9cd740f97fb1391e308cf65f3
itsre3 <saturdayobokparomaxwell@gmail.com>
2023-01-23 21:37:28 +0000

    Flask errors

--- dist/ba_root/mods/web/webst.py
+++ dist/ba_root/mods/web/webst.py
@@ -2,14 +2,22 @@
 import ba
 import _ba
 import _thread
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
+
+html_file = os.path.join(_ba.env()['python_directory_user'], "web" + os.sep) + "templates/stats_page.html"
+
 
 
 @app.route("/")
 def index():
-    return render_template("stats_page.html")
+    with open(html_file, 'r' as file:
+        contents = file.read()
+    
+    html = str(contents)
+    # return render_template("stats_page.html")
+    return make_response(html)
 
 def run():
     _thread.start_new_thread(app.run, ("0.0.0.0", 5000, False))
