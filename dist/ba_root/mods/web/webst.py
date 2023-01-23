fc249959c47ac1b4da9bc6486bf420faccca65ec
itsre3 <saturdayobokparomaxwell@gmail.com>
2023-01-23 20:19:46 +0000

    Flask errors

--- dist/ba_root/mods/web/webst.py
+++ dist/ba_root/mods/web/webst.py
@@ -1,14 +1,19 @@
 
-import asyncio
-import threading
+import ba
+import _ba
+import os
 import _thread
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+stats_dir = os.path.join(_ba.env()['python_directory_user'], "stats" + os.sep
+
+stats_file = stats_dir + "stats_page.html"
+
 @app.route("/")
 def index():
-    return render_template("stats/stats_page.html")
+    return render_template(stats_file)
 
 def run():
     _thread.start_new_thread(app.run, ("0.0.0.0", 5000, False))
