899192de87559a73b012d469480eb6f2e2d32b27
itsre3 <105688454+itsre3@users.noreply.github.com>
2023-04-25 10:57:40 +0000

    update webst

--- dist/ba_root/mods/web/webst.py
+++ dist/ba_root/mods/web/webst.py
@@ -2,33 +2,15 @@
 import ba
 import _ba
 import _thread, os
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-html_file = os.path.join(_ba.env()['python_directory_user'], "web" + os.sep) + "templates/stats_page.html"
-
-contents = ""
-
-def reload_file():
-    with open(html_file, 'r') as file:
-        content = file.read()
-    
-    contents = str(content)
-
-def tr():
-    r = ba.Timer(10, ba.Call(reload_file), repeat=True)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
-    
-    html = contents
-    # return render_template("stats_page.html")
-    return make_response(html)
+    return render_template("stats_page.html")
 
 def run():
     _thread.start_new_thread(app.run, ("0.0.0.0", 5000, False))
-    
-    _thread.start_new_thread(tr, ())
 
