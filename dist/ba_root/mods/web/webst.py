6a90d651dde27cec5520c94e921c12f67b1d52c4
itsre3 <saturdayobokparomaxwell@gmail.com>
2024-03-24 05:03:46 +0100

    revert

--- dist/ba_root/mods/web/webst.py
+++ dist/ba_root/mods/web/webst.py
@@ -1,20 +1,18 @@
 
 import ba
 import _ba
-import asyncio, threading
+import _thread, os
 import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
-logging.getLogger('werkzurg').disabled = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+logging.getLogger('werkzurg').disabled = True
 
 @app.route("/")
 def index():
     return render_template("stats_page.html")
 
 def run():
-    loop = asyncio.get_event_loop()
-    loop.create_task(app.run(debug=False))
-    threading.Thread(target=loop.run_forever).start()
+    _thread.start_new_thread(app.run, ("0.0.0.0", 5000, False))
 
