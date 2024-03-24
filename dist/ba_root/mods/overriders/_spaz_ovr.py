8db743b6f62d4918a6dbf9d2d8150d8cbe9ca26a
itsre3 <saturdayobokparomaxwell@gmail.com>
2024-03-24 11:32:55 +0100

    Support for effects

--- dist/ba_root/mods/overriders/_spaz_ovr.py
+++ dist/ba_root/mods/overriders/_spaz_ovr.py
@@ -45,7 +45,8 @@ class SurroundFactory(object):
 class ProSurroundBall(ba.Actor):
     def __init__(self, spaz, shape="bones"):
         ba.Actor.__init__(self)
-        self.spaz_ref = spaz
+        newspaz = spaz
+        self.spaz_ref = weakref.ref(newspaz)
         self.source_player = spaz
         
         factory = self.getFactory()
